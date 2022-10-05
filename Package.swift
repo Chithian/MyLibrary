1ed7d38d58396bb1ea21f17e6abbb2f161a1aab5
chithian <chithiansamony@gmail.com>
2022-10-05 10:59:14 +0700

    fix

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@
 import PackageDescription
 
 let package = Package(
-    name: "MyLibrary2",
+    name: "MyLibrary",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "MyLibrary2",
+            name: "MyLibrary",
             targets: ["MyLibrary"]),
     ],
     dependencies: [
@@ -19,7 +19,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "MyLibrary2",
+            name: "MyLibrary",
             dependencies: []),
       
     ]
