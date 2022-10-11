245d6eae20af5e8328e934494448b001902fec74
chithian <chithiansamony@gmail.com>
2022-10-11 17:11:23 +0700

    change file and add only build module file

--- Sources/Networking/File.swift
+++
@@ -1,9 +0,0 @@
-
-import Foundation
-
-public struct Networking {
-    public private(set) var text = "Calling API!"
-
-    public init() {
-    }
-}
---
+++ Sources/Networking/NetworkingModule.framework/Headers/NetworkingModule.h
@@ -0,0 +1,18 @@
+//
+//  NetworkingModule.h
+//  NetworkingModule
+//
+//  Created by Samony Chithian on 11/10/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NetworkingModule.
+FOUNDATION_EXPORT double NetworkingModuleVersionNumber;
+
+//! Project version string for NetworkingModule.
+FOUNDATION_EXPORT const unsigned char NetworkingModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetworkingModule/PublicHeader.h>
+
+
